d2103ead4c5cb1bd17cb2ff91743a61334d01ec0
Pranav-Srinivas-S <pranavsrinivas2000@gmail.com>
2021-03-05 10:47:42 +0530

    gitDemo

---
+++ myjenkinsapplication/src/com/capg/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.capg;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		for(int i=0; i<10; i++)
+			System.out.println("Hello friends from GitHub");
+		
+	//	*Time		*Day		*MonthDay		*Month		*WeekDay
+	//	30			9-18		1-15			1-12		6-7		
+	
+	}
+
+}
